699042d6503c23f84b681db650f6cf1891fc3fbc
namanofficial10 <75782506+namanofficial10@users.noreply.github.com>
2021-05-04 09:57:47 +0530

    Create rationalop.c

---
+++ rationalop.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(){
+
+    int a,b;
+
+    printf("------------------------------Logical Operators------------------------------\n");
+
+    printf("Enter First Number:");
+    scanf("%d",&a);
+    printf("Enter Second Number:");
+    scanf("%d",&b);
+
+    printf("a<b : %d",a<b);
+
+
+
+    return 0;
+}
