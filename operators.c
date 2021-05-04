6f85f577d9a8f0a75f21cd7ce54af1a8eb037724
namanofficial10 <75782506+namanofficial10@users.noreply.github.com>
2021-05-04 09:43:30 +0530

    Create operators.c

---
+++ operators.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+
+int main()
+{
+
+    int a,b,c,d,e;
+    float f;
+
+    printf("------------------------------Operators------------------------------\n");
+
+    printf("Enter first number:");
+    scanf("%d",&a);
+    printf("Enter second number:");
+    scanf("%d",&b);
+
+    c=a+b;
+    d=a-b;
+    e=a*b;
+    f=a/b;
+
+    printf("\nSum=%d",c);
+    printf("\nSubtraction=%d",d);
+    printf("\nMultiplication=%d",e);
+    printf("\nDivision=%f",f);
+    return 0;
+}
