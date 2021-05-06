40c3a7848c42248463e0d18a7f56a59bf9f5d82a
namanofficial10 <75782506+namanofficial10@users.noreply.github.com>
2021-05-06 16:34:55 +0530

    Update clock.c

--- clock.c
+++ clock.c
@@ -6,15 +6,15 @@ int main()
 {
   int i,j,k;
 
-
-  for(i=1;i<2;i++)
+while(i=1){
+  for(i=0;i<24;i++)
   {
-   for(j=1;j<=2;j++)
+   for(j=0;j<60;j++)
    {
-      for(k=1;k<=60;k++)
+      for(k=0;k<60;k++)
      {
-      printf("h:m:s\n");
-      printf("%d:%d:%d\n",i,j,k);
+      printf("\th:m:s\n");
+      printf("\t%d:%d:%d\n",i,j,k);
       Sleep(1000);
         system("cls");
      }
@@ -22,6 +22,8 @@ int main()
 
 
   }
+i==1;
 
+}
   return 0;
 }
